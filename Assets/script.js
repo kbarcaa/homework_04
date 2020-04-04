01686a79ad67f785288ac4b38437d038ec6964fd
SungJin Kim <sungjinkimm@gmail.com>
2020-04-03 23:56:26 -0400

    Done. maybe

--- Assets/script.js
+++ Assets/script.js
@@ -1,9 +1,4 @@
 // Assignment Code
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7"];
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", ""];
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", ];
-var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-"];
-
 
 var generateBtn = document.querySelector("#generate");
 
@@ -13,32 +8,39 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
+
+var h1Tag = document.getElementById("password")
+
 
 function getPwOptions(){
   // Asking pw length. switched string --> numbers by 'parseInt' 
   var pwLength = parseInt(prompt("Please enter the desired length for the password"));
   if(pwLength < 8 || pwLength > 128) {
     alert("Password must be bewtween 8 and 128 characters");
+    var pwLength = parseInt(prompt("Please enter the desired length for the password"));
+    while (pwLength < 8 || pwLength > 128) {
+      alert("Password must be bewtween 8 and 128 characters")
+      var pwLength = parseInt(prompt("Please enter the desired length for the password"));
+    };
   }
-  else if (pwLength >= 9 && pwLength < 129){
-    confirm("The length of your password will be: " + pwLength);
+  else if (pwLength >= 8 || pwLength < 129){
+    alert("The length of your password will be: " + pwLength);
     console.log("The length of your password will be: " + pwLength);
   };
 
-  var pwLower = confirm("Do you want lower case alphabets in your password?");
-  if (pwLower.toLowercase === true) {
-    alert("The password will include lower case alphabets");
-    console.log("The password will include lower case alphabets.")
+  var number = confirm("Do you want numbers in your password?");
+  if (number === true) {
+    alert("The password will include numbers");
+    console.log("The password will include numbers.")
   }
   else {
-    alert("The password will NOT include lower case alphabets.");
-    console.log("The password will NOT include lower case alphabets.");
+    alert("The password will NOT include numbers.");
+    console.log("The password will NOT include numbers.");
   };
 
   var pwUpper = confirm("Do you want upper case alphabets in your password?");
-  if (pwUpper.toLowercase === true) {
+  if (pwUpper === true) {
     alert("The password will include upper case alphabets");
     console.log("The password will include upper case alphabets.")
   }
@@ -55,12 +57,132 @@ function getPwOptions(){
   else {
     alert("The password will NOT include special characters.");
     console.log("The password will NOT include special characters.");
-  };
 };
 
-getPwOptions();
+
+// Combo1: Password with all 3 options
+if (number === true && pwUpper === true && pwSpecialChar === true){
+
+  function generate(pwLength) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789`~!@#$%^&*()-_=+';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < pwLength; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    console.log(result);
+    return result;
+ }
+ 
+ h1Tag.textContent = generate(pwLength);
+}
+
+// Combo2:Password with no number
+else if (number !== true && pwUpper === true && pwSpecialChar === true){
+
+  function generate(pwLength) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz`~!@#$%^&*()-_=+';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < pwLength; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    console.log(result);
+    return result;
+ };
+ h1Tag.textContent = generate(pwLength);
+}
+
+// Combo3: Password with no number && no upper case
+else if (number !== true && pwUpper !== true && pwSpecialChar === true){
+
+  function generate(pwLength) {
+    var result           = '';
+    var characters       = 'abcdefghijklmnopqrstuvwxyz`~!@#$%^&*()-_=+';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < pwLength; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    console.log(result);
+    return result;
+ };
+ h1Tag.textContent = generate(pwLength);
+}
+
+// Combo4: Password with no number && no special case
+else if (number !== true && pwUpper === true && pwSpecialChar !== true){
+
+  function generate(pwLength) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < pwLength; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    console.log(result);
+    return result;
+ };
+ h1Tag.textContent = generate(pwLength);
+}
+
+// Combo5: Password with no upper case
+else if (number === true && pwUpper !== true && pwSpecialChar === true){
+  
+  function generate(pwLength) {
+    var result           = '';
+    var characters       = 'abcdefghijklmnopqrstuvwxyz0123456789`~!@#$%^&*()-_=+';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < pwLength; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    console.log(result);
+    return result;
+ };
+ h1Tag.textContent = generate(pwLength);
+}
+
+// Combo6: Password with no upper case && no special character
+else if (number === true && pwUpper !== true && pwSpecialChar !== true){
+
+  function generate(pwLength) {
+    var result           = '';
+    var characters       = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < pwLength; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    console.log(result);
+    return result;
+ };
+ h1Tag.textContent = generate(pwLength);
+}
+
+// Combo7: Password with no special case
+else if (number === true && pwUpper === true && pwSpecialChar !== true){
+  
+  function generate(pwLength) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < pwLength; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    console.log(result);
+    return result;
+ };
+ 
+ h1Tag.textContent = generate(pwLength);
+} 
+
+// Combo 8: Nothing. Redo alert.
+else if (number !== true && pwUpper !== true && pwSpecialChar !== true){
+  alert("The password must contain at least one of the 3 options. Please try again.");
+  }
+}
+
+
+
+// getPwOptions();
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
+generateBtn.addEventListener("click", getPwOptions);
